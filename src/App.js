d872353d24abba8544f45ad3cb7755fff2033902
Liamhigh <36531077+Liamhigh@users.noreply.github.com>
2025-07-05 18:20:05 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import UploadPortal from './UploadPortal';
 
 function App() {
   return (
-    <div style={{ padding: 20, fontFamily: 'Arial' }}>
-      <h1>Verum Omnis Secure Upload</h1>
+    <div className="App">
       <UploadPortal />
     </div>
   );
